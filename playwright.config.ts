eb6892bd1bc135277f70df8f9839cb178813aeff
Karla <karlaramone@gmail.com>
2026-07-05 14:06:48 -0300

    build e deploy no vercel com secret correta

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL || 'https://velo-karlaramone.vercel.app',
+    baseURL: process.env.BASE_URL || 'https://automatizai-6yzpg7luw-karla-trancoso.vercel.app',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
